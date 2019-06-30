4aa3ad9105ca202e319512508d859f31254d612e
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-06-30 16:00:40 -0700

    passed the data retrived through props to the listApt component

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react'
 
 class ListAppointments extends Component {
     render() {
+        // const listItem = this.props.appointments.map(item =>(
+        //     <div>{item.petName} - {item.ownerName}</div>
+        //   ));
         return (
-            <div>
-                ListAppointments
+            <div className='appointment-list item-list mb-3'>
+                {this.props.appointments.map(item => ( 
+                    <div className='pet-item col media py-3'>
+                        <div className='mr-3'>
+                            <button className='pet-delete btn btn-sm btn-danger'>X</button>
+                        </div>
+
+                        <div className='pet-info media-body'>
+                            <div className='pet-head d-flex'>
+                                <spin className='pet-name'>{item.petName}</spin>
+                                <span className='apt-date ml-auto'>{item.aptDate}</span>
+                            </div>
+
+                            <div className='owner-name'>
+                                <span className='label-item'>Owner: </span>
+                                <span>{item.ownerName}</span>
+                            </div>
+
+                            <div className='apt-notes'>{item.aptNotes}</div>
+                        </div>
+                    </div>
+
+                ))}
+               
             </div>
-        )
+        );
     }
 }
 
