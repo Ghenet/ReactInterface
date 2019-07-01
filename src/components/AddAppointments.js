fdf7913b8ed21edccf168b4e204b08a25d8a96d0
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-07-01 14:01:55 -0700

    added a tracking form display state variable to display the add Apts form

--- src/components/AddAppointments.js
+++ src/components/AddAppointments.js
@@ -3,8 +3,68 @@ import React, { Component } from 'react'
 export default class AddAppointments extends Component {
     render() {
         return (
-            <div>
-            Add-Appointments 
+            <div className={'card textcenter mt-3 ' + (this.props.formDisplay ?'':'add-appointment' )}>
+                {/* adds the add-appointments class if fomDisplay is true  */}
+                <div className='apt-addheading card-header bg-primary text-white'>
+                    Add Appointment
+                </div>
+
+                <div className='card-body'>
+                    <form id='aptForm' noValidate onSubmit={this.handleAdd}>
+                        <div className='form-group form-row'>
+                            <label className='col-md-2 col-form-label text-md-right' htmlFor='petName'>
+                                Pet Name
+                            </label>
+                            <div className='col-md-10'>
+                                <input type='text' className='form-control' name='petName' placeholder="Pet's Name" />
+                            </div>
+                        </div>
+{/* Owners name */}
+                        <div className='form-group form-row'>
+                            <label className='col-md-2 col-form-label text-md-right' htmlFor='ownerName'>
+                                Pet Owner
+                            </label>
+                            <div className='col-md-10'>
+                                <input type='text' className='form-control' name='ownerName' placeholder="Owner's Name" />
+                            </div>
+                        </div>
+{/* Apt Date */}
+                        <div className='form-group form-row'>
+                            <label className='col-md-2 col-form-label text-md-right' htmlFor='aptDate'>
+                                Apt Date
+                            </label>
+                            <div className='col-md-4'>
+                                <input type='date' className='form-control' name='aptDate' placeholder="aptDate" />
+                            </div>
+                        </div>
+{/* Apt Time */}
+                        <div className='form-group form-row'>
+                            <label className='col-md-2 col-form-label text-md-right' htmlFor='aptTime'>
+                               Time
+                            </label>
+                            <div className='col-md-4'>
+                                <input type='time' className='form-control' name='aptTime' id='aptTime' />
+                            </div>
+                        </div>
+{/* //Apt Notes */}
+                        <div className='form-group form-row'>
+                            <label className='col-md-2 col-form-label text-md-right' htmlFor='ownerName'>
+                                Apt. Notes
+                            </label>
+                            <div className='col-md-10'>
+                                <textarea className='form-control' rows='4' cols='50' name='aptNotes' placeholder='Appointment Notes'/>
+                            </div>
+                        </div>
+{/* Submit button */}
+                        <div className='form-group form-row mb-0'>
+                            <div className='offset-md-2 col-md-10'>
+                                <button type='submit' className='btn btn-primary d-block ml-auto'>Add Appointment</button>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            
 
 
             </div>
